dda0aeceb7bee251d74f693cb09ab1695448f538
Sean Coady <sean.coady@lightstep.com>
2023-04-07 18:21:44 -0700

    More stuff

--- main.js
+++ main.js
@@ -24,13 +24,67 @@ async function getModels(apiKey) {
   }
 }
 
+function createOutputCard(output, rawOutput) {
+  const card = document.createElement("article");
+  card.classList.add("output-pane");
+
+  const cardTitle = document.createElement("h3");
+  cardTitle.textContent = "Completion Overview";
+  card.appendChild(cardTitle);
+
+  const cardBody = document.createElement("div");
+  card.appendChild(cardBody);
+
+  cardBody.appendChild(output);
+  cardBody.appendChild(rawOutput);
+
+  const clearButton = document.createElement("button");
+  clearButton.classList.add("button");
+  clearButton.textContent = "Clear";
+  clearButton.style.marginTop = "10px";
+  clearButton.addEventListener("click", () => {
+    outputContainer.removeChild(card);
+  });
+
+  cardBody.appendChild(clearButton);
+
+  outputContainer.appendChild(card);
+}
+
+
+function displayResults(result) {
+  const formattedOutput = document.createElement("p");
+  //formattedOutput.textContent = JSON.stringify(result);
+  console.log("result" + JSON.stringify(result) );
+
+
+  const performanceObject = {
+     timeToComplete : Math.floor((Date.now() / 1000) - result.created),
+     prompt_tokens : result.usage.prompt_tokens,
+     response_tokens : result.usage.completion_tokens,
+     total_tokens : result.usage.total_tokens,
+     model_used : result.model,
+     raw_result: result.choices[0].text
+  }
+
+
+  const rawOutputCard = document.createElement("pre");
+  rawOutputCard.style.whiteSpace = "pre-wrap";
+  //rawOutputCard.textContent = JSON.stringify(performanceObject, null, 2);
+  rawOutputCard.textContent = JSON.stringify(performanceObject,null,2)
+
+  createOutputCard(formattedOutput, rawOutputCard);
+}
+
+
+
+
 function createSpinner() {
     const spinner = document.createElement("span");
     spinner.classList.add("spinner");
     return spinner;
   }
   
-
   function showLoadingIndicator(tags) {
     tags.forEach(tag => {
       const card = document.createElement("article");
@@ -41,11 +95,18 @@ function createSpinner() {
       cardHeader.classList.add("card-header");
       cardHeader.setAttribute("data-tag", tag);
   
+      const checkMark = document.createElement("span");
+      checkMark.classList.add("checkmark");
+      checkMark.innerHTML = "&#10003;";
+      checkMark.style.display = "none";
+      cardHeader.appendChild(checkMark);
+  
       const cardText = document.createElement("p");
       cardText.classList.add("card-title");
+      cardText.style.display = "inline";
       cardText.textContent = tag;
-  
       cardHeader.appendChild(cardText);
+  
       cardHeader.appendChild(createSpinner());
   
       const cardBody = document.createElement("div");
@@ -66,14 +127,14 @@ function createSpinner() {
   
   
   
+  
   function findCardByHeader(tag) {
     const headers = document.querySelectorAll('header.card-header');
     let foundHeader = null;
   
     headers.forEach(header => {
-      if (header.querySelector('p.card-text').textContent.trim() === tag) {
-        console.log("Found matching header: " + header);
-        foundHeader = header;
+      if (header.getAttribute('data-tag') === tag) {
+        foundHeader = header.parentNode;
       }
     });
   
@@ -83,19 +144,16 @@ function createSpinner() {
   
 
   function updateLoadingIndicator(tag, isLoading) {
-    const tagHeader = outputContainer.querySelector(`header.card-header[data-tag="${tag}"]`);
-    if (tagHeader) {
-      const cardHeaderText = tagHeader.querySelector('.card-header > p.card-text');
-      if (isLoading) {
-        cardHeaderText.innerHTML = "Loading...";
-      } else {
-        const checkMark = document.createElement("span");
-        checkMark.classList.add("checkmark");
-        checkMark.innerHTML = "&#10003;";
-        checkMark.style.color = "green";
-        checkMark.style.animation = "checkmark-animation 1s";
-        tagHeader.replaceChild(checkMark, tagHeader.querySelector(".spinner"));
-      }
+    const cardHeader = findCardByHeader(tag);
+    const checkMark = cardHeader.querySelector(".checkmark");
+    const spinner = cardHeader.querySelector(".spinner");
+  
+    if (isLoading) {
+      spinner.style.display = "inline-block";
+      checkMark.style.display = "none";
+    } else {
+      spinner.style.display = "none";
+      checkMark.style.display = "inline-block";
     }
   }
   
@@ -116,43 +174,20 @@ function getTags() {
     return tags;
   }
   function showSummary(summary, tags) {
-    const outputContainer = document.querySelector("#output-container");
-    outputContainer.innerHTML = "";
-  
     for (const tag in summary) {
-      const card = document.createElement("article");
-      card.className = "card";
-  
-      const cardHeader = document.createElement("header");
-      cardHeader.className = "card-header";
-      const tagText = document.createTextNode(tag);
-      cardHeader.appendChild(tagText);
-      const checkMark = document.createElement("span");
-      checkMark.classList.add("checkmark");
-      checkMark.innerHTML = "&#10003;";
-      checkMark.style.display = "none";
-      cardHeader.appendChild(checkMark);
-      card.appendChild(cardHeader);
-  
-      const cardBody = document.createElement("div");
-      cardBody.className = "card__body";
+      const card = findCardByHeader(tag);
+      const cardBody = card.querySelector(".card-body");
       cardBody.textContent = summary[tag];
-      card.appendChild(cardBody);
-  
-      const cardFooter = document.createElement("footer");
-      cardFooter.className = "card__footer";
-      card.appendChild(cardFooter);
-  
-      outputContainer.appendChild(card);
     }
   
     tags.forEach(tag => {
       updateLoadingIndicator(tag, false);
-      const tagHeader = findCardByHeader(tag);
-      const checkMark = tagHeader.querySelector(".checkmark");
-      checkMark.style.display = "inline-block";
     });
   }
+  
+  
+  
+  
   
   
   
@@ -203,7 +238,11 @@ function getTags() {
         updateLoadingIndicator(tag, false);
       });
   
+      console.log(response);
       showSummary(summary, tags); // Pass tags as an argument
+      displayResults(data);
+
+    
     } catch (error) {
       console.error("Error:", error);
     }
@@ -229,20 +268,36 @@ function createTag(text) {
   document.addEventListener('DOMContentLoaded', () => {
     // ... Rest of your event listeners ...
   
-    modelDropdownButton.addEventListener("input", (event) => {
-        const filter = event.target.value.toUpperCase();
-        const dropdownContent = modelDropdown.querySelector(".dropdown-content");
-        const options = dropdownContent.getElementsByTagName("a");
-      
-        for (let i = 0; i < options.length; i++) {
-          const optionText = options[i].textContent || options[i].innerText;
-          if (optionText.toUpperCase().indexOf(filter) > -1) {
-            options[i].style.display = "";
-          } else {
-            options[i].style.display = "none";
-          }
+    modelDropdownButton.addEventListener('click', () => {
+      const filterInput = modelDropdown.querySelector("input");
+      const filter = filterInput.value.toUpperCase();
+      const dropdownContent = modelDropdown.querySelector("ul");
+      const options = dropdownContent.getElementsByTagName("li");
+      const label = modelDropdown.querySelector("label");
+    
+      if (filter && filter !== "Select Model") {
+        label.style.display = "none";
+      } else {
+        label.style.display = "block";
+      }
+    
+      for (let i = 0; i < options.length; i++) {
+        const optionText = options[i].textContent || options[i].innerText;
+        if (optionText.toUpperCase().indexOf(filter) > -1) {
+          options[i].style.display = "";
+        } else {
+          options[i].style.display = "none";
         }
-      });
+      }
+    });
+    
+    
+    
+    
+    
+    
+    
+    
 
 
     apiKeyInput.addEventListener('input', async () => {
