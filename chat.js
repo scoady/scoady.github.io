86ff76be69f2dce6738f935fc689103a9e2d131b
Sean Coady <sean.coady@lightstep.com>
2023-04-09 17:00:47 -0700

    Memory for AI Chat

--- chat.js
+++ chat.js
@@ -135,10 +135,28 @@ async function getModels(apiKey) {
   }
 }
 
+function generateUUID() {
+  var d = new Date().getTime();
+  if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+    d += performance.now(); // use high-precision timer if available
+  }
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+}
+
+
 async function handleInput() {
   const input = terminalTextarea.value;
-  history.push(input);
-  renderInput(`$ ${input}`);
+  const currentMessage = {
+    id: generateUUID(),
+    input: terminalTextarea.value,
+    parent_message_id: history.length > 0 ? history[history.length - 1].id : null
+  };
+  history.push(currentMessage);  
+  renderInput(`$ ${currentMessage.input}`);
   terminalOutput.scrollTop = terminalOutput.scrollHeight;
 
   if (input === "clear") {
@@ -164,8 +182,37 @@ async function handleInput() {
 
     renderOutput("",true);
     renderPrompt();
+    console.log("Selected model:", selectedModel);
     if (apiKey && selectedModel) {
       try {
+        messageID = generateUUID();
+        const messages = [];
+  
+        // Only add the system message if it's the first message in the conversation
+        if (history.length === 1) {
+          messages.push({
+            "role": "system",
+            "content": "\
+            You are a helpful assistant. You will retain message_id passed in through this conversation to recall contextual history. \
+            Additionally, you will find on future messages a parent_message_id,\
+            for additional contextual assistance. Message IDs and parent message IDs are encoded as metadata using a custom separator (===META===) and curly brackets following the actual content. \
+            Please only send your response without including these metadata in the output. This should be transparent to the user. \
+            Reply in the following format: \
+            Response \
+            ",
+          });
+          
+          
+        }
+  
+        const recentHistory = history.slice(-25);
+        recentHistory.forEach((msg) => {
+          messages.push({
+            "role": "user",
+            "content": `${msg.input} ===META==={MessageID: ${msg.id}, Parent MessageID: ${msg.parent_message_id ? msg.parent_message_id : 'None'}}`,
+          });
+        });
+  
         const response = await fetch("https://api.openai.com/v1/chat/completions", {
           method: "POST",
           headers: {
@@ -174,15 +221,7 @@ async function handleInput() {
           },
           body: JSON.stringify({
             model: selectedModel,
-            messages: [
-              {
-                "role": "system",
-                "content": "You are a helpful assistant."
-              },
-              {
-                "role": "user",
-                "content": input
-              }],
+            messages: messages,
             stream: true,
           }),
         });
@@ -194,6 +233,7 @@ async function handleInput() {
           throw new Error("Request failed");
         }
       } catch (error) {
+        console.log(error);
         renderOutput("[ GPT ] There was an error processing your request. Please try again.");
       }
     } else if (apiKey) {
@@ -211,26 +251,27 @@ async function handleInput() {
 
 
 function handleKeydown(event) {
-  if (event.keyCode === 13) {
+  if (event.keyCode === 13) { // Enter key
     event.preventDefault();
     handleInput();
   } else if (event.keyCode === 38) { // Up arrow key
     event.preventDefault();
     if (historyIndex > 0) {
       historyIndex--;
-      terminalTextarea.value = history[historyIndex];
+      terminalTextarea.value = history[historyIndex].input;
     }
   } else if (event.keyCode === 40) { // Down arrow key
     event.preventDefault();
     if (historyIndex < history.length - 1) {
       historyIndex++;
-      terminalTextarea.value = history[historyIndex];
+      terminalTextarea.value = history[historyIndex].input;
     } else {
       historyIndex = history.length;
       terminalTextarea.value = "";
     }
   }
 }
+
 
 function init() {
   renderPrompt();
